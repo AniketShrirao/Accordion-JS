34a9725b975c965409a972ead06534bc9c15c3da
saniketprdxn <saniketprdxn@github.com>
2020-03-03 11:32:17 +0530

    js active function added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,25 @@
 /* Author:Aniket */
 
+var accHead = document.getElementsByClassName('panelHead');
+var accContent = document.getElementsByClassName('panelContent');
 
+function firstActive() {
+  accHead[0].classList.add('active');
+  accContent[0].classList.add('open');
+}
+firstActive();
 
-
-
-
-
-
+for (i = 0; i < accHead.length; i++) {
+    accHead[i].addEventListener('click', toggleItem, false);
+}
+function toggleItem() {
+    var panelIsClosed = this.parentNode.classList.contains('close');
+    for (i = 0; i < accContent.length; i++) {
+        accContent[i].classList.remove('open');
+        accContent[i].classList.add('close');
+    }
+    if (panelIsClosed) {
+      this.parentNode.classList.remove('close');
+      this.parentNode.classList.add('open');
+    }
+}

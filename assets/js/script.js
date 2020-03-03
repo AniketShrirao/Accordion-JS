4b745b1de5cf038bb70e4560adf821037a958a4e
saniketprdxn <saniketprdxn@github.com>
2020-03-03 12:53:38 +0530

    js completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,41 @@
 /* Author:Aniket */
 
-var accHead = document.getElementsByClassName('panelHead');
-var accContent = document.getElementsByClassName('panelContent');
+function Accordion() {
+  var accHead = document.getElementsByClassName('panelHead');
+  var accContent = document.getElementsByClassName('panelContent');
+  var j = 0;
+  var previousActive;
 
-function firstActive() {
-  accHead[0].classList.add('active');
-  accContent[0].classList.add('open');
-}
-firstActive();
+  function firstActive() {
+    accHead[0].classList.add('active');
+    accContent[0].classList.add('open');
+  }
+  firstActive();
 
-for (i = 0; i < accHead.length; i++) {
-    accHead[i].addEventListener('click', toggleItem, false);
-}
-function toggleItem() {
-    var panelIsClosed = this.parentNode.classList.contains('close');
+  for (i = 0; i < accHead.length; i++) {
+      accHead[i].addEventListener('click', toggleItem, false);
+  }
+    function toggleItem() {
+    for(var head in accHead) {
+      if(head == 'length') {
+        break;
+      }
+      accHead[head].classList.remove('active');
+    }
+    var panelIsOpen = this.parentNode.classList.contains('close');
+    var panelIsClosed = this.parentNode.classList.contains('panel');
     for (i = 0; i < accContent.length; i++) {
-        accContent[i].classList.remove('open');
-        accContent[i].classList.add('close');
+      if(previousActive) {
+        accHead[i].classList.remove('active');        
+      }
+      this.classList.add('active');
+      accContent[i].classList.remove('open');
+      accContent[i].classList.add('close');
     }
-    if (panelIsClosed) {
-      this.parentNode.classList.remove('close');
-      this.parentNode.classList.add('open');
+    if (panelIsClosed || panelIsOpen) {
+      this.nextElementSibling.classList.remove('close');
+      this.nextElementSibling.classList.add('open');
     }
+  }
 }
+Accordion();
